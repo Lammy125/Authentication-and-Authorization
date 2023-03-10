d9fc1b5f4bc8945a7d3ca9972ad4481387807901
Adeyemo Peter <45455927+Lammy125@users.noreply.github.com>
2023-03-10 08:51:59 +0100

    try catch statement

--- config/database.js
+++ config/database.js
@@ -6,22 +6,29 @@ const password = "Lamide125";
 
 exports.connect = () => {
   // Connecting to the database
-  mongoose
-    .connect(
-      `mongodb+srv://Lammy125:${password}@cluster0.tnjqhy1.mongodb.net/?retryWrites=true&w=majority`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        //   useCreateIndex: true,
-        //   useFindAndModify: false,
-      }
-    )
-    .then(() => {
-      console.log("Successfully connected to database");
-    })
-    .catch((error) => {
-      console.log("database connection failed. exiting now...");
-      console.error(error);
-      process.exit(1);
-    });
+  mongoose.connect(
+    `mongodb+srv://Lammy125:${password}@cluster0.tnjqhy1.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //   useCreateIndex: true,
+      //   useFindAndModify: false,
+    }
+  );
+
+  try {
+    console.log("Successfully connected to database");
+  } catch (error) {
+    console.log("database connection failed. exiting now...");
+    console.error(error);
+    process.exit(1);
+  }
+  // .then(() => {
+  //   console.log("Successfully connected to database");
+  // })
+  // .catch((error) => {
+  //   console.log("database connection failed. exiting now...");
+  //   console.error(error);
+  //   process.exit(1);
+  // });
 };
